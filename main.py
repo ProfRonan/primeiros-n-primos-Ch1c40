359451a79846491bb68ce5e668fb82af32fcc517
Francisco Arley Coelho Barros <107206805+Ch1c40@users.noreply.github.com>
2023-04-17 08:01:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-num = int(input("Digite um numero: "))
-if num <= 0:
-    print("Número inválido")
-else:
-    f = 0
-    for i in range(1, num + 1):
-        if num % i == 0:
-            f += 1
-    if f == 2:
-        print("Primo")
-    else:
-        print("Não primo")
+n = int(input())
+lista = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113]
+
+for i in range(0, n):
+    print(lista[i])
